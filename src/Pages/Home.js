24d99babae1bd67c025de4742a56b5b49b47c9ca
dontech09 <donnjondanmakal@gmail.com>
2021-11-20 08:59:12 +0530

    First Commit �

---
+++ src/Pages/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <h1>Don Jose</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo ducimus,
+        eos facilis voluptatibus non corporis quasi ex sint modi sapiente fuga
+        aut hic, consequuntur esse fugiat! Perspiciatis non officiis doloribus!
+      </p>
+    </div>
+  );
+};
+
+export default Home;
